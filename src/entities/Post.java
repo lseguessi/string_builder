8c94c9495106455ec360885de78c2108ad18b7d0
Lucas Seguessi <lucas.seguessi@gmail.com>
2019-12-06 19:01:23 -0300

    Estudo finalizado

---
+++ src/entities/Post.java
@@ -0,0 +1,86 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	public Post() {}
+
+	public Post(Date moment, String tittle, String contetn, Integer likes) {
+		this.moment = moment;
+		this.title = tittle;
+		this.content = contetn;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTittle() {
+		return title;
+	}
+
+	public void setTittle(String tittle) {
+		this.title = tittle;
+	}
+
+	public String getContetn() {
+		return content;
+	}
+
+	public void setContetn(String contetn) {
+		this.content = contetn;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+	
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments: \n");
+		for (Comment c : comments) {
+			sb.append(c.getText()+ "\n");
+		}
+		return sb.toString();
+	}
+	
+}
